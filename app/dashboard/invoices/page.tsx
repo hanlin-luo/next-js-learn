d2ed603a39101a1d8a0b51cd247687e6f19e6154
hanlin <hanlin.luo2@gmail.com>
2024-07-20 01:30:29 +0800

    feat: Improve UI for invoices page with search functionality and pagination
    - Update the search component to include an onChange event handler that triggers a debounced search callback.
    - Add pagination component to display the total number of pages and allow navigation between them.
    - Implement search functionality in the handleSearch function, updating the URL query parameters and triggering a page reload.
    - Refactor the invoices page to include the search component and pagination component.
    - Update the CreateInvoice button to use a darker background color on hover.

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -1,3 +1,39 @@
-export default function Page() {
-  return <p>Invoices</p>;
+import Pagination from "@/app/ui/invoices/pagination";
+import Search from "@/app/ui/search";
+import Table from "@/app/ui/invoices/table";
+import { CreateInvoice } from "@/app/ui/invoices/buttons";
+import { lusitana } from "@/app/ui/fonts";
+import { InvoicesTableSkeleton } from "@/app/ui/skeletons";
+import { Suspense } from "react";
+import { fetchInvoicesPages } from "@/app/lib/data";
+
+// TODO: 这里有问题，页面是阻塞的动态页面，加载很慢页面要修改成同步的，但是就需要修改Pagination的实现方式
+export default async function Page({
+  searchParams,
+}: {
+  searchParams?: {
+    query?: string;
+    page?: number;
+  };
+}) {
+  const query = searchParams?.query || "";
+  const currentPage = searchParams?.page || 1;
+  const totalPages = await fetchInvoicesPages(query);
+  return (
+    <div className="w-full">
+      <div className="flex w-full items-center justify-between">
+        <h1 className={`${lusitana.className} text-2xl`}>Invoices</h1>
+      </div>
+      <div className="mt-4 flex items-center justify-between gap-2 md:mt-8">
+        <Search placeholder="Search invoices..." />
+        <CreateInvoice />
+      </div>
+      <Suspense key={query + currentPage} fallback={<InvoicesTableSkeleton />}>
+        <Table query={query} currentPage={currentPage} />
+      </Suspense>
+      <div className="mt-5 flex w-full justify-center">
+        <Pagination totalPages={totalPages} />
+      </div>
+    </div>
+  );
 }
